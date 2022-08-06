5280b98d573822567576af74aff29786df0e5535
NeoGreen22 <110316003+NeoGreen22@users.noreply.github.com>
2022-08-06 11:55:14 -0400

    Update prop_s.lua

--- prop_s.lua
+++ prop_s.lua
@@ -1,43 +1,49 @@
-VORP = exports.vorp_inventory:vorp_inventoryApi()
+local gumCore = {}
+TriggerEvent("getCore",function(core)
+    gumCore = core
+end)
+
+Inventory = exports.gum_inventory:gum_inventoryApi()
+gum = exports.gum_core:gumAPI()
 
 
-VORP.RegisterUsableItem("cigarette", function(data)
-	VORP.subItem(data.source, "cigarette", 1)
+Inventory.RegisterUsableItem("cigarette", function(data)
+	Inventory.subItem(data.source, "cigarette", 1)
 	TriggerClientEvent('prop:cigarettes', data.source)
 end)
 
-VORP.RegisterUsableItem("hairpomade", function(data)
-	VORP.subItem(data.source, "hairpomade", 1)
+Inventory.RegisterUsableItem("hairpomade", function(data)
+	Inventory.subItem(data.source, "hairpomade", 1)
 	TriggerClientEvent('prop:hairpomade', data.source)
 end)
 
 
-VORP.RegisterUsableItem("cigar", function(data)
-	VORP.subItem(data.source, "cigar", 1)
+Inventory.RegisterUsableItem("cigar", function(data)
+	Inventory.subItem(data.source, "cigar", 1)
 	TriggerClientEvent('prop:cigar', data.source)
 end)
 
-VORP.RegisterUsableItem("notebook", function(data)
+Inventory.RegisterUsableItem("notebook", function(data)
 	TriggerClientEvent('prop:ledger', data.source)
 end)
 
-VORP.RegisterUsableItem("pocket_watch", function(data)
+Inventory.RegisterUsableItem("pocket_watch", function(data)
 	TriggerClientEvent('prop:watch', data.source)
 end)
 
-VORP.RegisterUsableItem("book", function(data)
+Inventory.RegisterUsableItem("book", function(data)
 	TriggerClientEvent('prop:book', data.source)
 end)
 
-VORP.RegisterUsableItem("pipe", function(data)
+Inventory.RegisterUsableItem("pipe", function(data)
 	TriggerClientEvent('prop:pipe', data.source)
 end)
 
-VORP.RegisterUsableItem("fan", function(data)
+Inventory.RegisterUsableItem("fan", function(data)
 	TriggerClientEvent('prop:fan', data.source)
 end)
 
-VORP.RegisterUsableItem("chewingtobacco", function(data)
+Inventory.RegisterUsableItem("chewingtobacco", function(data)
 	VORP.subItem(data.source, "chewingtobacco", 1)
 	TriggerClientEvent('prop:chewingtobacco', data.source)
 end)
